1d2fff01e7a68e0241678f8ad38027f889a60a98
Ningú <n1ngu@riseup.net>
2021-09-06 11:25:53 +0200

    Drop external dependencies on bash and jq
    The ip-ranges.atlassian.com json response can be entirely
    parsed with terraform builtin functions. This relies in the
    fact that IPv4 and IPv6 addresses will either contain dots
    or colons but never both.
    
    This is very convenient for execution environments with limited
    external tools, like the very hashicorp/terraform docker images.
    
    Also document some interesting example usages.

--- main.tf
+++ main.tf
@@ -7,8 +7,14 @@ data "http" "bitbucket_ips" {
   }
 }
 
-# Format response to separate IPv4 and IPv6.
-data "external" "ip_addresses" {
-  program = ["bash", "-c", "echo '${data.http.bitbucket_ips.body}' | jq '{ipv4:[.items[]|select(.cidr|contains(\":\")|not).cidr]|join(\",\"),ipv6:[.items[]|select(.cidr|contains(\":\")).cidr]|join(\",\")}'"]
+locals {
+  ip_range = tolist(jsondecode(data.http.bitbucket_ips.body).items[*].cidr)
+  ipv4_range = compact([
+    for cidr in local.ip_range :
+    replace(cidr, "/.*[:].*/", "")
+  ])
+  ipv6_range = compact([
+    for cidr in local.ip_range :
+    replace(cidr, "/.*[.].*/", "")
+  ])
 }
-
--- output.tf
+++ output.tf
@@ -1,9 +1,24 @@
-output "ipv4_ip_addresses" {
+output "ip_range" {
+  description = "A list of IPv4 and IPv6 Addresses for outbound connections from Bitbucket's services."
+  value       = local.ip_range
+}
+
+output "ipv4_range" {
   description = "A list of IPv4 Addresses for outbound connections from Bitbucket's services."
-  value       = split(",", data.external.ip_addresses.result.ipv4)
+  value       = local.ipv4_range
+}
+
+output "ipv6_range" {
+  description = "A list of IPv6 Addresses for outbound connections from Bitbucket's services."
+  value       = local.ipv6_range
+}
+
+output "ipv4_ip_addresses" {
+  description = "Alias to ipv4_range"
+  value       = local.ipv4_range
 }
 
 output "ipv6_ip_addresses" {
-  description = "A list of IPv6 Addresses for outbound connections from Bitbucket's services."
-  value       = split(",", data.external.ip_addresses.result.ipv6)
+  description = "Alias to ipv6_range"
+  value       = local.ipv6_range
 }
